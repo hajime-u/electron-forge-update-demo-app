107ed3530f7520ece4c63b993d12e3fb72059982
Hajime <hajime-u@users.noreply.github.com>
2024-12-29 23:28:44 +0900

    add upgrade settings for github release

--- forge.config.ts
+++ forge.config.ts
@@ -52,6 +52,17 @@ const config: ForgeConfig = {
       [FuseV1Options.OnlyLoadAppFromAsar]: true,
     }),
   ],
+  publishers: [
+    {
+      name: "@electron-forge/publisher-github",
+      config: {
+        repository: {
+          owner: "hajime-u",
+          name: "electron-forge-update-demo-app",
+        },
+      },
+    },
+  ],
 };
 
 export default config;
